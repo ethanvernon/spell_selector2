664ab80926d9ecd7732c13d9b727ab6b685d13f0
Ethan Vernon <ethan.vernon@colorado.edu>
2019-01-23 14:55:19 +0800

    Updates

---
+++ precache-manifest.db19aaa0ae42d0d48ad338aa00ca82e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b3c385c07122fb5efde",
+    "url": "/static/js/main.4b3c385c.chunk.js"
+  },
+  {
+    "revision": "ff16dc5c9b06eba80a2a",
+    "url": "/static/js/1.ff16dc5c.chunk.js"
+  },
+  {
+    "revision": "4b3c385c07122fb5efde",
+    "url": "/static/css/main.553213a5.chunk.css"
+  },
+  {
+    "revision": "ff16dc5c9b06eba80a2a",
+    "url": "/static/css/1.29883c29.chunk.css"
+  },
+  {
+    "revision": "caaa71fa5fc37ec968df0c5fd1986584",
+    "url": "/index.html"
+  }
+];
