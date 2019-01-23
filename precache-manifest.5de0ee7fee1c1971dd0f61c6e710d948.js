a0f419a2122bf27cc7057bdf7d1b0357b7881d9e
Ethan Vernon <ethan.vernon@colorado.edu>
2019-01-23 15:28:30 +0800

    Updates

---
+++ precache-manifest.5de0ee7fee1c1971dd0f61c6e710d948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "527a9582b50a0f6d58f6",
+    "url": "/spell_selector2/static/js/runtime~main.527a9582.js"
+  },
+  {
+    "revision": "9c736013ab718b622c81",
+    "url": "/spell_selector2/static/js/main.9c736013.chunk.js"
+  },
+  {
+    "revision": "55b6b319761ffe74802a",
+    "url": "/spell_selector2/static/js/1.55b6b319.chunk.js"
+  },
+  {
+    "revision": "9c736013ab718b622c81",
+    "url": "/spell_selector2/static/css/main.90b53d29.chunk.css"
+  },
+  {
+    "revision": "55b6b319761ffe74802a",
+    "url": "/spell_selector2/static/css/1.278b49c7.chunk.css"
+  },
+  {
+    "revision": "9095a35e8655e9b342bced06deb25c5c",
+    "url": "/spell_selector2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e35808a528df5cadb612d2dae9724f3f.js"
+  "/spell_selector2/precache-manifest.5de0ee7fee1c1971dd0f61c6e710d948.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/spell_selector2/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
